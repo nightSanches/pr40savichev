c80809e10491008209ab46cc2eb3dcd1d30254fc
nightSanches <galesavich@gmail.com>
2024-05-27 16:56:40 +0500

    Разработанное перенаправление на страницу с предметами. Савичевв

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pr37savichev.Controllers
+{
+	public class HomeController : Controller
+	{
+		public RedirectResult Index()
+		{
+			return Redirect("Items/List");
+		}
+	}
+}
