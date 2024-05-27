193aa2b7b3b4d8b516951d067d26f971f829030a
nightSanches <galesavich@gmail.com>
2024-05-27 17:30:08 +0500

    Выполненная работа на оценку удовлетворительно. Савичев

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -43,6 +43,17 @@ namespace pr37savichev.Controllers
             }
             return Json(Startup.BasketItem);
         }
+        public ActionResult BasketCount(int idItem = -1, int count = -1)
+        {
+            if (idItem != -1)
+            {
+                if (count == 0) 
+                    Startup.BasketItem.Remove(Startup.BasketItem.Find(x => x.Id == idItem));
+                else 
+                    Startup.BasketItem.Find(x => x.Id == idItem).Count = count;
+            }
+            return Json(Startup.BasketItem);
+        }
 
         [HttpGet]
         public ViewResult Add()
